3b487c12843682b49b9c9d03537f57244e99b408
amentrak <181474@student.umons.ac.be>
2019-10-28 10:32:34 +0100

    va cherchedr la reponse du prof

--- src/test/java/exercise2/Exercise2Test.java
+++ src/test/java/exercise2/Exercise2Test.java
@@ -1,5 +1,6 @@
 package exercise2;
 
+import exercise1.ExceptionWhenValueIs42;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -43,7 +44,7 @@ public class Exercise2Test {
 	}
 
 	@Test
-	public void save_and_load() {
+	public void save_and_load() throws IOException, ClassNotFoundException {
 		Classroom classroom = new Classroom(TEACHER, STUDENTS);
 		Exercise2.save(classroom, tempFilePath);
 		Classroom loadedClassroom = Exercise2.load(tempFilePath);
